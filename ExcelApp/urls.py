bbc1dbafdaa7832b46a7f2d9c1aa64eb04261ce8
Schofield <Hunter.Schofield@york.ca>
2017-08-16 09:06:28 -0400

    basic setup and simple gui for testing

--- ExcelApp/urls.py
+++ ExcelApp/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'ExcelApp'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^details/$', views.details, name='details'),
+    #url(r'^details/(?P<pid>[0-9]+)/$', views.getReport, name='getReport'),
+    url(r'^getReport/$', views.getReport, name='getReport'),
+    #url(r'^ExcelApp/$', views.details, name='details'),
 ]
--- ExcelApp/views.py
+++ ExcelApp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-
+from django.template import loader
+from django.urls import reverse
 from django.http import HttpResponse
-
+import requests
 
 def index(request):
-    return HttpResponse("Excel App index")
+	#template_name = 'ExcelApp/main.html'
+	#template = loader.get_template('ExcelApp/main.html')
+	return render(request, 'ExcelApp/index.html')
+	#return HttpResponse(template.render(request))
+
+def details(request):
+	return render(request, 'ExcelApp/main.html')
+
+
+def getReport(request):
+	rid = request.POST['rid']
+	
+	if rid == '2':
+		response = requests.get('http://ykr-dev-apex.devyork.ca/apexenv/bsmart_data/stp_ws/stp_pop_test/')
+		return HttpResponse(response)
+		#return render(request, 'ExcelApp/index.html')
+	else:
+		return render(request, 'ExcelApp/main.html')
