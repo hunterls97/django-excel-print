ce3d67266564a0078600c4c1094fb72726c22c28
hunter liam <hunter.schofield@york.ca>
2017-08-22 15:24:50 -0400

    changes made before they got mad at me

--- ExcelApp/reports.py
+++ ExcelApp/reports.py
@@ -179,9 +179,19 @@ class reports(object):
 			'align': 'left',
 		})
 
+		item_format.set_text_wrap()
+
+		subtotal_format = workbook.add_format({
+			'font_name':'Calibri',
+			'font_size': 14,
+			'font_color':'black',
+			'align':'left',
+			'bg_color':'#D3D3D3',
+		})
+
 		data = res
 
-		worksheet.set_column('A:G', 25)
+		worksheet.set_column('A:G', 35)
 		worksheet.set_row(0,36)
 		worksheet.set_row(1,36)
 		worksheet.merge_range('A1:G2','Natural Heritage and Forestry Division, Environmental Services Department',main_header1_format)
@@ -190,7 +200,7 @@ class reports(object):
 		worksheet.merge_range('A4:G4','CON#'+ year +'-Street Tree Planting and Establishment Activities',main_header2_format)
 		worksheet.merge_range('A5:G5',title,title_format)
 		worksheet.merge_range('A6:G6',' ')
-		item_fields = ['Item', 'Quantity', 'Last Year Price', 'This Year Price', 'This Year Actual', 'Estimated Total', 'Total']
+		item_fields = ['Item', 'Quantity', 'Last Year Price', 'This Year Estimate', 'This Year Actual', 'Estimated Total', 'Total']
 
 		programs = {'Capital Infrastructure' : [],
 		'Infill / Retrofit' : [],
@@ -199,11 +209,11 @@ class reports(object):
 		#separates items by programs
 		for idx, val in enumerate(data["items"]):
 			if data["items"][idx]["program"] == "Capital Infrastructure":
-				programs['Capital Infrastructure'].append(list(data["items"][idx].values()))
+				programs['Capital Infrastructure'].append(data["items"][idx])
 			elif data["items"][idx]["program"] == "Infill / Retrofit":
-				programs['Infill / Retrofit'].append(list(data["items"][idx].values()))
+				programs['Infill / Retrofit'].append(data["items"][idx])
 			else:
-				programs['EAB Replacement'].append(list(data["items"][idx].values()))
+				programs['EAB Replacement'].append(data["items"][idx])
 
 		cr = 7
 
@@ -215,7 +225,6 @@ class reports(object):
 				worksheet.write_row('A' + str(cr+1), item_fields, item_header_format)
 				cr += 2
 
-			mainItems = {'A' : [], 'B' : [], 'C' : [], 'D' : [], 'E' : [], 'F' : []}
 			miDict = {'A' : 'Tree Planting - Ball and Burlap Trees',
 			'B' : 'Tree Planting - Potted Perennials and Grass',
 			'C' : 'Tree Planting - Potted Shrubs',
@@ -223,18 +232,46 @@ class reports(object):
 			'E' : 'Stumping',
 			'F' : 'Watering'}
 
-			for idx, val in enumerate(programs[program]):
-				mainItems[programs[program][idx][1]].append(list(programs[program][idx])) #programs[program][idx][0] is type_id
+			#print(programs)
+			items = {'A' : {}, 'B' : {}, 'C' : {}, 'D' : {}, 'E' : {}, 'F' : {}}
 
-			for idx, val in enumerate(mainItems):
-				if mainItems[val]:
+			#this is good
+			for idx, val in enumerate(programs[program]):
+				if "item" in programs[program][idx]:
+					if not programs[program][idx]["item"] in items[programs[program][idx]["sec"]]:
+						items[programs[program][idx]["sec"]].update({programs[program][idx]["item"] : [int(programs[program][idx]["quantity"]),
+							programs[program][idx]["lyp"] if "lyp" in programs[program][idx] else 0,
+							programs[program][idx]["pe"] if "pe" in programs[program][idx] else 0,
+							programs[program][idx]["up"] if "up" in programs[program][idx] else 0]})
+					else:
+						items[programs[program][idx]["sec"]][programs[program][idx]["item"]][0] += int(programs[program][idx]["quantity"])
+
+			for idx, val in enumerate(items):
+				if items[val]:
 					worksheet.merge_range('A' + str(cr) + ':G' + str(cr), miDict[val], subtitle_format)
 					cr += 1
-					for idx2, val2 in enumerate(mainItems[val]):
-						worksheet.write_row('A' + str(cr), mainItems[val][idx2], item_format)
+					start = cr
+					for idx2, val2 in enumerate(items[val]):
+						d = [val2]
+						d.extend(items[val][val2])
+						worksheet.write_row('A' + str(cr), d, item_format)
+						worksheet.write_formula('F' + str(cr), '=B' + str(cr) + '*D' + str(cr), item_format)
+						worksheet.write_formula('G' + str(cr), '=B' + str(cr) + '*E' + str(cr), item_format)
 						cr += 1
 
-		worksheet.merge_range('A6:G6',' ')
+					worksheet.write('A' + str(cr), 'SubTotal: ', subtotal_format)
+					worksheet.write_formula('B' + str(cr), '=SUM(B' + str(start) + ':B' + str(cr-1) + ')', subtotal_format)
+					worksheet.write('C' + str(cr), '', subtotal_format)
+					worksheet.write('D' + str(cr), '', subtotal_format)
+					worksheet.write('E' + str(cr), '', subtotal_format)
+					worksheet.write_formula('F' + str(cr), '=SUM(F' + str(start) + ':F' + str(cr-1) + ')', subtotal_format)
+					worksheet.write_formula('G' + str(cr), '=SUM(G' + str(start) + ':G' + str(cr-1) + ')', subtotal_format)
+
+					worksheet.merge_range('A' + str(cr+1) + ':G' + str(cr+1),' ')
+					cr += 2
+
+		print(getWateringItems(2014))
+
 
 		#print(programs)
 
